dce2c4632a57a638ac1580f624cce6feda948824
Jigo <joubertjr.abbey@gmail.com>
2026-09-16 07:16:22 -0400

    Initial HexWorks Fabric ecosystem setup

---
+++ hexworks-core/src/main/java/dev/hexworks/core/HexWorksCore.java
@@ -0,0 +1,13 @@
+package dev.hexworks.core;
+
+import net.fabricmc.api.ModInitializer;
+
+public class HexWorksCore implements ModInitializer {
+
+    public static final String MOD_ID = "hexworks-core";
+
+    @Override
+    public void onInitialize() {
+        System.out.println("[HexWorks Core] Initialized.");
+    }
+}
